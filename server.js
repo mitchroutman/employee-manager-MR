ea1176e02875b886386f784e43182dae56812e92
Mitchell Routman <mitchell.routman@gmail.com>
2022-02-22 16:41:23 -0500

    function outlines for server queries

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const mysql = require('mysql2');
+const fs = require('fs');
+const inquirer = require('inquirer');
 
 const PORT = process.env.PORT || 3001;
-const app = express();
-const api = 
+const app = express(); 
 
 //middleware
 app.use(express.urlencoded({extended: false}));
@@ -22,7 +23,56 @@ const db = mysql.createConnection(
 
 //
 
+function viewAllEmployees() {
+    let query = "SELECT * FROM employee"
+    
+    return allEmployees;
+};
 
-app.use((req, res) => {res.status(404).end()});
+function addEmployee() {
+    let query = "INSERT INTO employee (id, first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?, ?";
 
-app.listen(PORT, () => {console.log(`Server running on port ${PORT}`)});
+    return
+};
+
+function updateEmployeeRole() {
+    query = "";
+};
+
+function viewAllRoles() {
+    query = "";
+    
+    return roles;
+};
+
+function addRole() {
+    query = "";
+
+    return
+};
+
+function viewAllDepartments() {
+    let query = "";
+
+    return allDepartments;
+};
+
+function addDepartment() {
+    query ="";
+
+    return
+};
+
+function quit() {
+    return
+};
+
+//
+
+app.use((req, res) => {
+    res.status(404).end()
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+});
